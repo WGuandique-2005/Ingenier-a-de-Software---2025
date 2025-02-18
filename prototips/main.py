83b9920724cc54699c180215a4fcdd668fa1e98c
WGuandique-2005 <wguandiquerivera07@gmail.com>
2025-02-18 15:16:31 -0600

    commit

--- prototips/main.py
+++ prototips/main.py
@@ -8,7 +8,7 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template('cards.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
